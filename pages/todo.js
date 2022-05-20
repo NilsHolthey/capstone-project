9cbdbbb9537b19501776a39121768a5fd011020c
NilsHolthey <nils.holthey@gmx.de>
2022-05-20 10:11:19 +0200

    add test

--- pages/todo.js
+++ pages/todo.js
@@ -1,17 +1,30 @@
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import Form from '../src/components/Form/Form';
 import TodosGrid from '../src/components/Todos/TodosGrid';
-
 import { getTodos } from '../src/services/get-todos';
 
 export function getStaticProps() {
-	const todos = getTodos();
+	const initialTodos = getTodos();
 
 	return {
 		props: {
-			todos,
+			initialTodos,
 		},
 	};
 }
 
-export default function Todo({ todos }) {
-	return <TodosGrid todos={todos} />;
+export default function Home({ initialTodos }) {
+	const [todos, setTodos] = useState(initialTodos);
+
+	const addTodo = newdata => {
+		setTodos([...todos, { id: nanoid(), status: 'doIt', title: newdata.title }]);
+		console.log(newdata);
+	};
+	return (
+		<>
+			<TodosGrid todos={todos} />
+			<Form onAddTodo={addTodo} />
+		</>
+	);
 }
