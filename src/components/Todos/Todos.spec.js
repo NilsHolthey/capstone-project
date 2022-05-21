9b0a8d83d7ba412be678aaeecccd55c72f79814b
NilsHolthey <nils.holthey@gmx.de>
2022-05-21 15:54:08 +0200

    Add test

--- src/components/Todos/Todos.spec.js
+++ src/components/Todos/Todos.spec.js
@@ -1,6 +1,7 @@
 import Todo from './Todo';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { DeleteButton } from '../UI/Button/DeleteButton.styled';
 
 describe('Displays the title of a single todo', () => {
 	it('renders the title', () => {
@@ -9,5 +10,15 @@ describe('Displays the title of a single todo', () => {
 		const todos = screen.getByText(/Capstone Project/i);
 
 		expect(todos).toBeInTheDocument();
+	});
+});
+
+describe('Display DeleteButton', () => {
+	it('renders the DeleteButton in each listitem', () => {
+		render(<DeleteButton />);
+
+		const deleteButton = screen.getByRole('button');
+
+		expect(deleteButton).toBeInTheDocument();
 	});
 });
