644840f50b147b57406ec6768fb7a2cdc5ca605f
NilsHolthey <nils.holthey@gmx.de>
2022-05-22 14:34:46 +0200

    remove form from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import { nanoid } from 'nanoid';
 import { useEffect, useState } from 'react';
-import Form from '../src/components/Form/Form';
+
 import dynamic from 'next/dynamic';
 import { getTodos } from '../src/services/get-todos';
 import { loadFromLocal, saveToLocal } from '../src/components/lib/localStorage';
@@ -25,18 +24,9 @@ export default function Home({ initialTodos }) {
 		saveToLocal('localTodos', todos);
 	}, [todos]);
 
-	const addTodo = newdata => {
-		setTodos([...todos, { id: nanoid(), status: 'doIt', title: newdata.title }]);
-	};
-
 	const deleteTodo = id => {
 		setTodos(todos.filter(todo => todo.id !== id));
 	};
 
-	return (
-		<>
-			<TodosGrid todos={todos} onDeleteTodo={deleteTodo} />
-			<Form onAddTodo={addTodo} />
-		</>
-	);
+	return <TodosGrid todos={todos} onDeleteTodo={deleteTodo} />;
 }
