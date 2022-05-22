c7a6c5e7bb8c627b97b768dcd22bf491b6dcc19e
NilsHolthey <nils.holthey@gmx.de>
2022-05-22 14:41:13 +0200

    Add svg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import SvgIcon from '../SVGs/icons';
+import { AddButton } from '../UI/Button/AddButton.styled';
 
 export default function Footer() {
 	const router = useRouter();
 	return (
 		<footer>
 			<Link passHref href="/CreateTodo">
-				<button type="button" pathName={router.pathname}>
-					add
-				</button>
+				<AddButton type="button" pathName={router.pathname}>
+					<SvgIcon variant="add" size="40px" color="#6667ab" />
+				</AddButton>
 			</Link>
 		</footer>
 	);
---
+++ src/components/UI/Button/AddButton.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const AddButton = styled.button`
+	border: none;
+	background-color: white;
+`;
