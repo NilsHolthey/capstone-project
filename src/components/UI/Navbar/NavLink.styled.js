47bc308156cc35f8e17a4b6037e467c8e32cf24c
NilsHolthey <nils.holthey@gmx.de>
2022-05-23 09:18:36 +0200

    Add -webkit-

--- src/components/UI/Navbar/NavLink.styled.js
+++ src/components/UI/Navbar/NavLink.styled.js
@@ -4,7 +4,9 @@ export const NavLink = styled.a`
 	text-decoration: none;
 	font-weight: bolder;
 	color: ${props => (props.href === props.pathName ? '#6667ab' : '#545060')};
-	text-decoration: ${props => (props.href === props.pathName ? 'underline #6667ab 3px' : 'none')};
-	font-size: 16px;
+	text-decoration: ${props => (props.href === props.pathName ? 'underline #6667ab 3px' : '')};
+	-webkit-text-decoration: ${props =>
+		props.href === props.pathName ? 'underline #6667ab 3px' : ''};
 	text-underline-offset: 10px;
+	font-size: 16px;
 `;
